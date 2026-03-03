fbb27406930cf1f3b15c2dbb088d5f7a00d7c13a
Nyle <nyle.schwaller@epita.fr>
2026-03-03 16:16:27 +0000

    Ajoutez des fichiers projet.

---
+++ Library.Domain/Library.Domain.cpp
@@ -0,0 +1,10 @@
+// Library.Domain.cpp : Définit les fonctions de la bibliothèque statique.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Il s'agit d'un exemple de fonction de bibliothèque
+void fnLibraryDomain()
+{
+}
