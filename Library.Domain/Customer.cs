06217f8512bfe4dc8adfcc7f0a2a3a81445d68ed
Nyle <nyle.schwaller@epita.fr>
2026-03-03 17:57:25 +0000

    fin du cour

---
+++ Library.Domain/Customer.cs
@@ -0,0 +1,13 @@
+﻿using Library.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+
+    public List<Invoice> Invoices { get; set; } = new();
+}
---
+++ Library.Domain/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime InvoiceDate { get; set; } = DateTime.UtcNow;
+
+        // FK to Customer
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+        public List<InvoiceLine> Lines { get; set; } = new();
+    }
+}
